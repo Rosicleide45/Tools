d8633273f80f883505297c0054c9c8d690131a48
K Seven <32661918+k7brasil@users.noreply.github.com>
2022-08-13 12:58:04 -0300

    Create SSL.sh

---
+++ SSL.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+clear
+echo -e "UPDATING REPOSITORS, PLEASE WAIT..."
+sleep 3
+apt update; apt upgrade -y; clear
+echo -e "Installing zip file manager..."
+sleep 2
+apt install zip -y; apt install unzip -y
+cd /etc; rm -rf ssl stunnel; clear
+echo -e "Replacing SSL certificates..."
+sleep 3
+wget --no-check-certificate https://github.com/WebManagerSSH/Tools/blob/main/ssl.zip?raw=true
+unzip ssl.zip; rm ssl.zip; clear
+echo -e "SSL certificate update completed."
+echo -e "Restart the server for the changes "
+echo -e "take effect immediately!!"
+echo ""
+echo -e "Powered by WMSSH"
+cd; rm SSL.sh
